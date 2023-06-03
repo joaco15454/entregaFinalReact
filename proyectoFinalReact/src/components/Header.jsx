8cef88483dd33e08fd2af37873953fddcd8ac83f
Joaquin Hernandez Durand <joacohernandez157@gmail.com>
2023-06-03 18:08:34 -0300

    Finaliazacion del footer e implementacion del heero

--- proyectoFinalReact/src/components/Header.jsx
+++ proyectoFinalReact/src/components/Header.jsx
@@ -32,13 +32,76 @@ const Header = () => {
           </div>
           <div className="col-5">
             <div className="header-upper-links d-flex align-items-center justify-content-between">
-
+              <div>
+                <Link className='d-flex align-items-center text-white' style={{gap:'10px'}}>
+                <img src="images/compare.svg" alt="compare" />
+                <p className='mb-0'>
+                  Comparar productos
+                </p>
+                </Link>
+              </div>
+              <div>
+                <Link className='d-flex align-items-center text-white' style={{gap:'10px'}}>
+                <img src="images/wishlist.svg" alt="wishlist" />
+                <p className='mb-0'>Favoritos</p>
+                </Link>
+              </div>
+              <div>
+                <Link className='d-flex align-items-center text-white' style={{gap:'10px'}}>
+                <img src="images/user.svg" alt="user" />
+                <p className='mb-0'>Inicia sesion</p>
+                </Link>
+              </div>
+              <div>
+                <Link className='d-flex align-items-center text-white' style={{gap:'10px'}}>
+                <img src="images/cart.svg" alt="cart" />
+                <div className='d-flex flex-column' style={{gap:'10px'}}>
+                  <span className='badge bg-white text-dark'>
+                      0
+                  </span>
+                  <p className='mb-0'>$500</p>
+                </div>
+                </Link>
+              </div>
             </div>
 
           </div>
 
         </div>
       </div>
+    </header>
+    <header className='header-bottom py-3'>
+      <div className="container-xxl">
+        <div className="row">
+          <div className="col-12">
+            <div className="menu_bottom d-flex align-items-center" style={{gap:'30px'}}>
+              <div>
+              <div className="dropdown">
+                <button className="btn btn-secondary dropdown-toggle bg-transparent border-0 d-flex align-items-center  " style={{gap:'15px'}} type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                   <img src='images/menu.svg' alt='menu' /> <span className='me-5 d-inline-block'>Categorias</span>
+
+                </button>
+                <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                  <li><Link className="dropdown-item text-white" to="">Action</Link></li>
+                  <li><Link className="dropdown-item text-white" to="">Another action</Link></li>
+                  <li><Link className="dropdown-item text-white" to="">Something else here</Link></li>
+                </ul>
+              </div>
+              </div>
+              <div className='menu-links'>
+                <div className="d-flex align-items-center " style={{gap:'15px'}}>
+                  <NavLink   to="/">Inicio</NavLink>
+                  <NavLink  to="/">Tienda</NavLink>
+                  <NavLink  to="/">Novedades</NavLink>
+                  <NavLink  to="/">Contacto</NavLink>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+
     </header>
     </>
   )
--- proyectoFinalReact/src/pages/Home.jsx
+++ proyectoFinalReact/src/pages/Home.jsx
@@ -1,8 +1,112 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+    <section className="home-wrapper-1 py-5">
+      <div className="container-xxl">
+        <div className="row">
+          <div className="col-6">
+            <div className="main-banner position-relative">
+              <img src="images/main-banner-1.jpg" className='img-fluid rounded-3' alt="banner principal" />
+              <div className="main-banner-content position-absolute">
+                <h4>Grandes ofertas</h4>
+                <h5>IPHONE S13+ Pro</h5>
+                <p>Desde 455555#</p>
+                <Link className='button'>COMPRA AHORA</Link>
+              </div>
+            </div>
+          </div>
+          <div className="col-6">
+            <div className="d-flex flex-wrap justify-content-between align-items-center" style={{gap:'10px'}}>
+            <div className="small-banner position-relative">
+              <img src="images/catbanner-01.jpg" className='img-fluid rounded-3' alt="banner principal" />
+              <div className="small-banner-content position-absolute">
+                <h4>Grandes ofertas</h4>
+                <h5>IPHONE S13+ Pro</h5>
+                <p>Desde 455555#</p>
+
+              </div>
+            </div>
+            <div className="small-banner position-relative">
+              <img src="images/catbanner-02.jpg" className='img-fluid rounded-3' alt="banner principal" />
+              <div className="small-banner-content position-absolute">
+              <h4>Mejor llegado</h4>
+                <h5>Compra este relejo</h5>
+                <p>Desde 455555#</p>
+
+              </div>
+            </div>
+            <div className="small-banner position-relative">
+              <img src="images/catbanner-03.jpg" className='img-fluid rounded-3' alt="banner principal" />
+              <div className="small-banner-content position-absolute">
+                <h4>Recien llegado</h4>
+                <h5>Compra este relejo</h5>
+                <p>Desde 455555#</p>
+
+              </div>
+            </div>
+            <div className="small-banner position-relative">
+              <img src="images/catbanner-04.jpg" className='img-fluid rounded-3' alt="banner principal" />
+              <div className="small-banner-content position-absolute">
+                <h4>Recien llegado</h4>
+                <h5>Compra este relejo</h5>
+                <p>Desde 455555#</p>
+
+              </div>
+            </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </section>
+    <section className="home-wrapper-2 py-5">
+      <div className="container-xxl">
+        <div className="row">
+          <div className="col-12">
+            <div className="services d-flex align-items-center justify-content-between">
+                <div className='d-flex align-items-center' style={{gap:'10px'}}>
+                  <img src="images/service.png" alt="" />
+                  <div>
+                    <h6>Envio gratis</h6>
+                    <p className='mb-0'>Desde ordenes de 10000$ en adelante</p>
+                  </div>
+                </div>
+                <div className='d-flex align-items-center' style={{gap:'10px'}}>
+                  <img src="images/service-02.png" alt="" />
+                  <div>
+                    <h6>Ofertas diarias.</h6>
+                    <p className='mb-0'>Ahorra hasta un 50% de descuento.</p>
+                  </div>
+                </div>
+                <div>
+                  <img src="images/service-03.png" alt="" />
+                  <div>
+                    <h6>Soporte 24/7</h6>
+                    <p className='mb-0'>Comprale a expertos</p>
+                  </div>
+                </div>
+                <div>
+                  <img src="images/service-04.png" alt="" />
+                  <div>
+                    <h6>Precios accesibles</h6>
+                    <p className='mb-0'>Precios a costo.</p>
+                  </div>
+                </div>
+                <div>
+                  <img src="images/service-05.png" alt="" />
+                  <div>
+                    <h6>Pagos seguros.</h6>
+                    <p className='mb-0'> 100% seguros.</p>
+                  </div>
+                </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    </>
   )
 }
 
